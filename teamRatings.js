271007915e2e41d0dbf799794c1525a2954cb656
nikolai2244 <nikolairays@gmail.com>
2025-11-30 15:06:52 -0500

    Add files via upload

---
+++ teamRatings.js
@@ -0,0 +1,3 @@
+// generated
+export const TEAM_RATINGS = { "KC": {offense:88, defense:91, pace:62, qb:95, homeField:4} };
+export const DEFAULT_TEAM_RATING={offense:75, defense:75, pace:60, qb:80, homeField:2};
