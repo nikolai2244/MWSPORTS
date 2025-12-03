3834c59405c8eaaa41b36365f9759a192c8a61fa
nikolai2244 <nikolairays@gmail.com>
2025-12-03 06:44:28 -0500

    Create SimulateGame.js

---
+++ src/components/SimulateGame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SimulateGame() {
+  return (
+    <div className="simulate-game">
+      <div className="container">
+        <h2>Game Simulation</h2>
+        <p>Run advanced game simulations</p>
+      </div>
+    </div>
+  );
+}
+
+export default SimulateGame;
