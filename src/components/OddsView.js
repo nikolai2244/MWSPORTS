d6d81b4cfabc9b6ac22f661a62a7e79d63b708c8
nikolai2244 <nikolairays@gmail.com>
2025-12-03 06:42:21 -0500

    Create OddsView.js

---
+++ src/components/OddsView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function OddsView() {
+  return (
+    <div className="odds-view">
+      <div className="container">
+        <h2>Odds Comparison</h2>
+        <p>Compare odds across sportsbooks</p>
+      </div>
+    </div>
+  );
+}
+
+export default OddsView;
