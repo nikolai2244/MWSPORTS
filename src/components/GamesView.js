a1d631e3bc1d5518f9b1a790d15c899887991963
nikolai2244 <nikolairays@gmail.com>
2025-12-03 06:41:32 -0500

    Create GamesView.js

---
+++ src/components/GamesView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GamesView() {
+  return (
+    <div className="games-view">
+      <div className="container">
+        <h2>Today's Games</h2>
+        <p>View live games and odds</p>
+      </div>
+    </div>
+  );
+}
+
+export default GamesView;
