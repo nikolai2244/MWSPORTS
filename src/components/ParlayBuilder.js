a50fb5c353f10418652cf027a4f40a9c0f07e5c1
nikolai2244 <nikolairays@gmail.com>
2025-12-03 06:43:38 -0500

    Create ParlayBuilder.js

---
+++ src/components/ParlayBuilder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ParlayBuilder() {
+  return (
+    <div className="parlay-builder">
+      <div className="container">
+        <h2>Parlay Builder</h2>
+        <p>Build and analyze custom parlays</p>
+      </div>
+    </div>
+  );
+}
+
+export default ParlayBuilder;
