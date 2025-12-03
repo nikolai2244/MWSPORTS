1f1e60233b87cf401f4d6a141efaaac441268a4c
nikolai2244 <nikolairays@gmail.com>
2025-12-03 06:43:01 -0500

    Create PropsView.js

---
+++ src/components/PropsView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PropsView() {
+  return (
+    <div className="props-view">
+      <div className="container">
+        <h2>Player Props</h2>
+        <p>Analyze player prop bets</p>
+      </div>
+    </div>
+  );
+}
+
+export default PropsView;
